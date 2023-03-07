195127d9fbd6805df95f3df2d4400e4585df1c9e
TaliMotzkin <104077339+TaliMotzkin@users.noreply.github.com>
2023-03-07 11:08:05 +0200

    Update for_files.m

--- for_files.m
+++ for_files.m
@@ -5,11 +5,10 @@ myFiles = dir(fullfile(myDir, "*.mat")); %gets all mat files in struct
 for i = 1:length(myFiles)
     mat = load(myFiles(i).name, 'analysis');
     [ filepath , name , ext ] = fileparts(myFiles(i).name)
-    newStr = strcat(strrep(name,"EDA","SCL"), "_bin")
+    newStr = strcat(strrep(name,"EDA","SCL"), "_bin") %or mul
     xlswrite(newStr,mat.analysis.tonicData')
 end
 
-%Ledalab('C:\Users\Tali\Documents\study\work\seminar\sumer 2022\Braclets raw\bracelet raw\mat_binary_July\', 'open', 'text2', 'analyze','CDA', 'optimize',1)
 
 
 
